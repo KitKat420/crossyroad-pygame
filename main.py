57d82c5a36c2da139202c7dc76bfdf074e5cac41
KitKat420 <danteclark66@gmail.com>
2021-12-22 22:46:38 -0500

    added player to canvas

--- main.py
+++ main.py
@@ -5,15 +5,19 @@ pygame.init()
 clock = pygame.time.Clock()
 
 FPS = 60
-WIDTH, HEIGHT = (800, 500)
+WIDTH, HEIGHT = 800, 500
 WHITE = (255, 255, 255)
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Crossy Roads")
 
 
+player_img = pygame.transform.scale(
+    pygame.image.load("./Assets/pizza.png", "player"), (50, 50))
+
+
 def draw_window():
     pygame.Surface.fill(WINDOW, WHITE)
-
+    WINDOW.blit(player_img, (WIDTH / 2 - 25, HEIGHT - 50))
     pygame.display.update()
 
 
