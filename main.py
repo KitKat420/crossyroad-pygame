aa8e87e1e256680a4b0d120234273527c4bbd2c9
KitKat420 <danteclark66@gmail.com>
2021-12-23 22:48:12 -0500

    implemented score system

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import pygame
-from pygame import key
-from pygame import transform
 from pygame.constants import K_w
 import random
-import time
+from scoreboard import Scoreboard
+
+STEP = 5
+FPS = 60
+WIDTH, HEIGHT = 800, 500
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Crossy Roads")
+
 pygame.init()
+
+scoreboard = Scoreboard()
+
+font = pygame.font.Font("./Assets/PressStart2P-Regular.ttf", 25)
+game_over_text = font.render("GAME OVER", False, BLACK)
 
 image_paths = ["./Assets/tesla-model-bus.png",
                "./Assets/tesla-model-car.png",
@@ -21,14 +33,6 @@ for image in image_paths:
 
 clock = pygame.time.Clock()
 
-STEP = 5
-FPS = 60
-WIDTH, HEIGHT = 800, 500
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Crossy Roads")
-
 player_img = pygame.transform.scale(
     pygame.image.load("./Assets/pizza.png", "player"), (50, 50))
 bg_img = pygame.image.load("./Assets/mountain-bg.jpg", "mountain-bg")
@@ -39,13 +43,32 @@ def player_move(player, key_pressed):
         player.y -= STEP
 
 
+def home(player_rect):
+
+    player_rect.x = WIDTH / 2 - 50
+    player_rect.y = HEIGHT - 50
+
+
 def draw_window(player_surface, player_rect, cars):
+    scoreboard_text = font.render(
+        f"SCORE: {scoreboard.current_score}", False, BLACK)
     # drawing player using image asset and applying rect attributes to the image
     WINDOW.blit(bg_img, (0, 0))
     WINDOW.blit(player_surface, player_rect)
-
+    WINDOW.blit(scoreboard_text, (WIDTH / 2 -
+                scoreboard_text.get_width() / 2, 25))
     for car in cars:
         WINDOW.blit(car[0], car[1])
+
+        if car[1].collidepoint(player_rect.x, player_rect.y + 50):
+            WINDOW.blit(game_over_text, (WIDTH / 2 -
+                        game_over_text.get_width() / 2, 200))
+            scoreboard.current_score = 0
+            home(player_rect)
+
+        if player_rect.y == 0:
+            scoreboard.increase_score()
+            home(player_rect)
     pygame.display.flip()
 
 
@@ -58,6 +81,7 @@ def main():
     player_as_rect.x = WIDTH / 2 - 50
     player_as_rect.y = HEIGHT - 50
     my_event = pygame.USEREVENT + 1
+
     pygame.time.set_timer(my_event, 1000)
     while is_game_on:
         clock.tick(FPS)
@@ -73,10 +97,10 @@ def main():
             if event.type == pygame.QUIT:
                 is_game_on = False
             elif event.type == my_event:
-
                 cars.append(car)
 
         player_move(player_as_rect, pygame.key.get_pressed())
+
         draw_window(player_img, player_as_rect, cars)
 
         for car in cars:
